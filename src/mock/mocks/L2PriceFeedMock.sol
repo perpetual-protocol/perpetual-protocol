64d125f9e6ceb5959f42461c1dd420c2bb7f4a16
shao <wraecca@gmail.com>
2020-11-10 15:11:52 +0800

    fix missing func in mock contract

--- src/mock/mocks/L2PriceFeedMock.sol
+++ src/mock/mocks/L2PriceFeedMock.sol
@@ -16,6 +16,14 @@ contract L2PriceFeedMock {
         price = _price;
     }
 
+    function getPrice(bytes32) public view returns (uint256) {
+        return price;
+    }
+
+    function setPrice(uint256 _price) public {
+        price = _price;
+    }
+
     event PriceFeedDataSet(bytes32 key, uint256 price, uint256 timestamp, uint256 roundId);
 
     function setLatestData(
