ed2bdbfa32a5fe68c4e3c656c01211e9f97c3540
shao <wraecca@gmail.com>
2020-11-12 11:13:33 +0800

    use constants instead of process.env

--- scripts/deploy-layer.ts
+++ scripts/deploy-layer.ts
@@ -1,13 +1,21 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 // import { buidlerArguments, config, web3 } from "@nomiclabs/buidler"
+import { BuidlerRuntimeEnvironment } from "@nomiclabs/buidler/types"
 import { ConfigManager } from "@openzeppelin/cli"
 import { Wallet } from "ethers"
+import {
+    HOMESTEAD_MNEMONIC,
+    KOVAN_MNEMONIC,
+    RINKEBY_MNEMONIC,
+    ROPSTEN_MNEMONIC,
+    SOKOL_MNEMONIC,
+    XDAI_MNEMONIC,
+} from "../constants"
 import { ContractPublisher } from "../publish/ContractPublisher"
-import { AccountMetadata, SystemMetadataDao } from "../publish/SystemMetadataDao"
 import { OzScript } from "../publish/OzScript"
 import { SettingsDao } from "../publish/SettingsDao"
+import { AccountMetadata, SystemMetadataDao } from "../publish/SystemMetadataDao"
 import { Layer, Network, Stage } from "./common"
-import { BuidlerRuntimeEnvironment } from "@nomiclabs/buidler/types"
 
 export async function getFromAccount(bre: BuidlerRuntimeEnvironment): Promise<string> {
     const network = bre.buidlerArguments.network! as Network
@@ -15,22 +23,22 @@ export async function getFromAccount(bre: BuidlerRuntimeEnvironment): Promise<st
     if (network === "localhost") {
         return (await bre.web3.eth.getAccounts())[0]
     } else if (network === "ropsten") {
-        const wallet = Wallet.fromMnemonic(process.env["ROPSTEN_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(ROPSTEN_MNEMONIC || "")
         return wallet.address
     } else if (network === "kovan") {
-        const wallet = Wallet.fromMnemonic(process.env["KOVAN_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(KOVAN_MNEMONIC || "")
         return wallet.address
     } else if (network === "rinkeby") {
-        const wallet = Wallet.fromMnemonic(process.env["RINKEBY_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(RINKEBY_MNEMONIC || "")
         return wallet.address
     } else if (network === "homestead") {
-        const wallet = Wallet.fromMnemonic(process.env["HOMESTEAD_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(HOMESTEAD_MNEMONIC || "")
         return wallet.address
     } else if (network === "sokol") {
-        const wallet = Wallet.fromMnemonic(process.env["SOKOL_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(SOKOL_MNEMONIC || "")
         return wallet.address
     } else if (network === "xdai") {
-        const wallet = Wallet.fromMnemonic(process.env["XDAI_MNEMONIC"] || "")
+        const wallet = Wallet.fromMnemonic(XDAI_MNEMONIC || "")
         return wallet.address
     } else {
         throw new Error("network not found: " + network)
