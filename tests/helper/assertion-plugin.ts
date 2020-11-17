a5ff84055bcbb5dcc954e02a7e5361d9bfea576c
shao <wraecca@gmail.com>
2020-11-17 11:04:32 +0800

    delete commented assertion-helper legacy mocha plugin for contract event is no longer working

--- tests/helper/assertion-plugin.ts
+++ tests/helper/assertion-plugin.ts
@@ -24,35 +24,4 @@ export function assertionHelper(chai: Chai.ChaiStatic, utils: Chai.ChaiUtils): v
             }
         }
     })
-
-    // chai.Assertion.addMethod("emit", function(eventName: string) {
-    //     const obj = this._obj as Truffle.TransactionResponse
-    //     const log = obj.logs.find((_log: Truffle.TransactionLog) => _log.event === eventName)
-    //     const msg = `Expected event "${eventName}" to be emitted, but it doesn't exist in the contract.`
-    //     this.assert(log, msg, msg, eventName)
-    // })
-    // chai.Assertion.addMethod("withArgs", function(map: Record<string, any>) {
-    //     const obj = this._obj as Truffle.TransactionResponse
-    //     // map expected key/value pair from Record<string, any> to Record<string, string>
-    //     // to make diff clear
-    //     const expected: Record<string, string> = {}
-    //     Object.entries(map).forEach(([key, value]) => {
-    //         expected[key] = value.toString()
-    //     })
-    //     // same as expected to array of Record<string, string>
-    //     const actual: Record<string, string>[] = obj.logs.map(log => {
-    //         const filteredArgs: Record<string, any> = {}
-    //         Object.keys(log.args).forEach(key => {
-    //             if (Number.isNaN(Number.parseInt(key, 10)) && key !== "__length__") {
-    //                 filteredArgs[key] = log.args[key].toString()
-    //             }
-    //         })
-    //         return filteredArgs
-    //     })
-    //     const msg = "Expected event has certain args."
-    //     const expectedList = Object.entries(expected)
-    //     // expected one of item in actual to match every expected arguments
-    //     const result = actual.some(args => expectedList.every(([key, value]) => args[key] === value))
-    //     this.assert(result, msg, msg, [expected], actual)
-    // })
 }
