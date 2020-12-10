3c28666f43193d103208875e89bbe9ccab6e710a
Yuren Ju <yurenju@gmail.com>
2020-12-10 17:05:06 +0800

    feat: make amm flatten

--- buidler.flatten.amm.config.ts
+++ buidler.flatten.amm.config.ts
@@ -3,14 +3,16 @@ import originalConfig from "./buidler.config"
 import { SRC_DIR_NAME } from "./constants"
 import { FLATTEN_BASE_DIR } from "./scripts/flatten"
 
+const dirName = "Amm"
+
 const config = {
     ...originalConfig,
     paths: {
         // source & artifacts does not work since we use openzeppelin-sdk for upgradable contract
-        sources: join(FLATTEN_BASE_DIR, SRC_DIR_NAME),
-        artifacts: join(FLATTEN_BASE_DIR, originalConfig.paths.artifacts),
-        tests: join(FLATTEN_BASE_DIR, "./tests"),
-        cache: join(FLATTEN_BASE_DIR, "./cache"),
+        sources: join(FLATTEN_BASE_DIR, dirName, SRC_DIR_NAME),
+        artifacts: join(FLATTEN_BASE_DIR, dirName, originalConfig.paths.artifacts),
+        tests: join(FLATTEN_BASE_DIR, dirName, "./tests"),
+        cache: join(FLATTEN_BASE_DIR, dirName, "./cache"),
     },
 }
 
