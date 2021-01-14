a810f69e4e94d8545a98e3b1406f0db77f645a3b
shao <wraecca@gmail.com>
2021-01-14 15:35:17 +0800

    separate the openzeppelin config files by stage

--- scripts/deploy-layer.ts
+++ scripts/deploy-layer.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { ethers } from "@nomiclabs/buidler"
 import { BuidlerRuntimeEnvironment } from "@nomiclabs/buidler/types"
+import { mkdir, mv, test } from "shelljs"
 import { ContractPublisher } from "../publish/ContractPublisher"
 import { SettingsDao } from "../publish/SettingsDao"
 import { SystemMetadataDao } from "../publish/SystemMetadataDao"
-import { AccountMetadata, Layer, Network, Stage } from "./common"
+import { AccountMetadata, Layer, Network, ozNetworkFile, Stage } from "./common"
+import { getOpenZeppelinDir } from "./path"
 
 export async function deployLayer(
     stage: Stage,
@@ -27,5 +29,26 @@ export async function deployLayer(
     console.log(`deployer=${address}`)
     const publisher = new ContractPublisher(layerType, settingsDao, systemMetadataDao)
 
-    await publisher.publishContracts(batch)
+    // workaround: when deploying staging/production to the same network (eg. xdai)
+    // we don't want to overwrite the .openzeppelin config file
+    // so we'll keep the original file in our own dir (separate by different stage).
+    const ozSettingsFileName = ozNetworkFile[network]
+    const stagePath = `${getOpenZeppelinDir()}/${stage}`
+    const sourceFile = `${stagePath}/${ozSettingsFileName}.json`
+    const destinationFile = `${getOpenZeppelinDir()}/${ozSettingsFileName}.json`
+
+    // 1. before deploying, copy from source to destination first
+    // 2. during deploying, open zeppelin sdk will update the destination file
+    // 3. after deploying, we'll copy the updated destination files back to the dir managed by us
+    if (!test("-e", sourceFile)) {
+        mkdir("-p", stagePath)
+    } else {
+        mv(sourceFile, destinationFile)
+    }
+
+    try {
+        await publisher.publishContracts(batch)
+    } finally {
+        mv(destinationFile, sourceFile)
+    }
 }
