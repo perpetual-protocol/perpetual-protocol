a34e783bbe321dfc71459ac0f11c3de9858d7b1e
tienshaoku <tienshaoku@gmail.com>
2021-02-08 19:30:59 +0800

    fix: revert & skip negative price.

--- src/ChainlinkPriceFeed.sol
+++ src/ChainlinkPriceFeed.sol
@@ -89,15 +89,15 @@ contract ChainlinkPriceFeed is IPriceFeed, PerpFiOwnableUpgrade, BlockContext {
         AggregatorV3Interface aggregator = getAggregator(_priceFeedKey);
         requireNonEmptyAddress(address(aggregator));
 
-        (, int256 latestPrice, , , ) = aggregator.latestRoundData();
-        return formatDecimals(regulateChainlinkPrice(latestPrice), priceFeedDecimalMap[_priceFeedKey]);
+        (, uint256 latestPrice, ) = getLatestRoundData(aggregator);
+        return formatDecimals(latestPrice, priceFeedDecimalMap[_priceFeedKey]);
     }
 
     function getLatestTimestamp(bytes32 _priceFeedKey) external view override returns (uint256) {
         AggregatorV3Interface aggregator = getAggregator(_priceFeedKey);
         requireNonEmptyAddress(address(aggregator));
 
-        (, , , uint256 latestTimestamp, ) = aggregator.latestRoundData();
+        (, , uint256 latestTimestamp) = getLatestRoundData(aggregator);
         return latestTimestamp;
     }
 
@@ -121,28 +121,25 @@ contract ChainlinkPriceFeed is IPriceFeed, PerpFiOwnableUpgrade, BlockContext {
         //         base           current previous now
 
         uint8 decimal = priceFeedDecimalMap[_priceFeedKey];
-        (uint80 round, int256 latestPrice, , uint256 latestTimestamp, ) = aggregator.latestRoundData();
+        (uint80 round, uint256 latestPrice, uint256 latestTimestamp) = getLatestRoundData(aggregator);
         uint256 baseTimestamp = _blockTimestamp().sub(_interval);
         // if latest updated timestamp is earlier than target timestamp, return the latest price.
         if (latestTimestamp < baseTimestamp || round == 0) {
-            return formatDecimals(regulateChainlinkPrice(latestPrice), decimal);
+            return formatDecimals(latestPrice, decimal);
         }
 
         // rounds are like snapshots, latestRound means the latest price snapshot. follow chainlink naming
         uint256 previousTimestamp = latestTimestamp;
         uint256 cumulativeTime = _blockTimestamp().sub(previousTimestamp);
-        uint256 weightedPrice = regulateChainlinkPrice(latestPrice).mul(cumulativeTime);
+        uint256 weightedPrice = latestPrice.mul(cumulativeTime);
         while (true) {
             if (round == 0) {
                 // if cumulative time is less than requested interval, return current twap price
                 return formatDecimals(weightedPrice.div(cumulativeTime), decimal);
             }
 
-            // @audit - it seems to rely on roundId being monotonic increasing (step=1), but it is not the case
-            //  per Chainlink docs, section "Why don’t the rounds update chronologically?" (https://docs.chain.link/docs/faq) (@detoo)
             round = round - 1;
-            // get current round timestamp and price
-            (, int256 currentPrice, , uint256 currentTimestamp, ) = aggregator.getRoundData(round);
+            (, uint256 currentPrice, uint256 currentTimestamp) = getRoundData(aggregator, round);
 
             // check if current round timestamp is earlier than target timestamp
             if (currentTimestamp <= baseTimestamp) {
@@ -150,33 +147,31 @@ contract ChainlinkPriceFeed is IPriceFeed, PerpFiOwnableUpgrade, BlockContext {
                 // now is 1000, _interval is 100, then target timestamp is 900. If timestamp of current round is 970,
                 // and timestamp of NEXT round is 880, then the weighted time period will be (970 - 900) = 70,
                 // instead of (970 - 880)
-                weightedPrice = weightedPrice.add(
-                    regulateChainlinkPrice(currentPrice).mul(previousTimestamp.sub(baseTimestamp))
-                );
+                weightedPrice = weightedPrice.add(currentPrice.mul(previousTimestamp.sub(baseTimestamp)));
                 break;
             }
 
             uint256 timeFraction = previousTimestamp.sub(currentTimestamp);
-            weightedPrice = weightedPrice.add(regulateChainlinkPrice(currentPrice).mul(timeFraction));
+            weightedPrice = weightedPrice.add(currentPrice.mul(timeFraction));
             cumulativeTime = cumulativeTime.add(timeFraction);
             previousTimestamp = currentTimestamp;
         }
         return formatDecimals(weightedPrice.div(_interval), decimal);
     }
 
-    function getPreviousPrice(bytes32 _priceFeedKey, uint256 _numOfRoundBack) public view override returns (uint256) {
+    function getPreviousPrice(bytes32 _priceFeedKey, uint256 _numOfRoundBack) external view override returns (uint256) {
         AggregatorV3Interface aggregator = getAggregator(_priceFeedKey);
         requireNonEmptyAddress(address(aggregator));
 
         (uint80 round, , , , ) = aggregator.latestRoundData();
         require(round > 0 && round >= _numOfRoundBack, "Not enough history");
-        (, int256 currentPrice, , uint256 currentTimestamp, ) =
-            aggregator.getRoundData(round - uint80(_numOfRoundBack));
-        return formatDecimals(regulateChainlinkPrice(currentPrice), priceFeedDecimalMap[_priceFeedKey]);
+        (, int256 previousPrice, , , ) = aggregator.getRoundData(round - uint80(_numOfRoundBack));
+        requirePositivePrice(previousPrice);
+        return formatDecimals(uint256(previousPrice), priceFeedDecimalMap[_priceFeedKey]);
     }
 
     function getPreviousTimestamp(bytes32 _priceFeedKey, uint256 _numOfRoundBack)
-        public
+        external
         view
         override
         returns (uint256)
@@ -186,8 +181,54 @@ contract ChainlinkPriceFeed is IPriceFeed, PerpFiOwnableUpgrade, BlockContext {
 
         (uint80 round, , , , ) = aggregator.latestRoundData();
         require(round > 0 && round >= _numOfRoundBack, "Not enough history");
-        (, , , uint256 currentTimestamp, ) = aggregator.getRoundData(round - uint80(_numOfRoundBack));
-        return currentTimestamp;
+        (, int256 previousPrice, , uint256 previousTimestamp, ) =
+            aggregator.getRoundData(round - uint80(_numOfRoundBack));
+        requirePositivePrice(previousPrice);
+        return previousTimestamp;
+    }
+
+    //
+    // INTERNAL VIEW FUNCTIONS
+    //
+
+    function getLatestRoundData(AggregatorV3Interface _aggregator)
+        internal
+        view
+        returns (
+            uint80,
+            uint256 finalPrice,
+            uint256
+        )
+    {
+        (uint80 round, int256 latestPrice, , uint256 latestTimestamp, ) = _aggregator.latestRoundData();
+        finalPrice = uint256(latestPrice);
+        if (latestPrice < 0) {
+            requireEnoughHistory(round);
+            (round, finalPrice, latestTimestamp) = getRoundData(_aggregator, round - 1);
+        }
+        return (round, finalPrice, latestTimestamp);
+    }
+
+    function getRoundData(AggregatorV3Interface _aggregator, uint80 _round)
+        internal
+        view
+        returns (
+            uint80,
+            uint256,
+            uint256
+        )
+    {
+        (uint80 round, int256 latestPrice, , uint256 latestTimestamp, ) = _aggregator.getRoundData(_round);
+        while (latestPrice < 0) {
+            requireEnoughHistory(round);
+            round = round - 1;
+            (, latestPrice, , latestTimestamp, ) = _aggregator.getRoundData(round);
+        }
+        return (round, uint256(latestPrice), latestTimestamp);
+    }
+
+    function formatDecimals(uint256 _price, uint8 _decimals) internal pure returns (uint256) {
+        return _price.mul(TOKEN_DIGIT).div(10**uint256(_decimals));
     }
 
     //
@@ -198,15 +239,12 @@ contract ChainlinkPriceFeed is IPriceFeed, PerpFiOwnableUpgrade, BlockContext {
         require(_addr != address(0), "empty address");
     }
 
-    //
-    // INTERNAL VIEW FUNCTIONS
-    //
-    function regulateChainlinkPrice(int256 _price) internal pure returns (uint256) {
-        // we don't support negative price, so there will be a lower bound at 0
-        return (_price > 0) ? uint256(_price) : 0;
+    function requireEnoughHistory(uint80 _round) internal pure {
+        require(_round > 0, "Not enough history");
     }
 
-    function formatDecimals(uint256 _price, uint8 _decimals) internal pure returns (uint256) {
-        return _price.mul(TOKEN_DIGIT).div(10**uint256(_decimals));
+    function requirePositivePrice(int256 _price) internal pure {
+        // a negative price should be reverted to prevent an extremely large/small premiumFraction
+        require(_price > 0, "Negative price");
     }
 }
