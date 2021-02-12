9e4c54b309d10062b136448fc9c5c8977af6bf17
shao <wraecca@gmail.com>
2021-02-12 14:14:39 +0800

    refactor statusCheck

--- scripts/StatusCheck.ts
+++ scripts/StatusCheck.ts
@@ -7,6 +7,8 @@ import ERC20Artifact from "../build/contracts/ERC20.json"
 import InsuranceFundArtifact from "../build/contracts/InsuranceFund.json"
 import { Amm, ClearingHouse, ERC20, InsuranceFund } from "../types/ethers"
 
+// TODO move to another standalone repo with SystemTest
+
 const USDC_DECIMALS = 6
 const provider = new ethers.providers.JsonRpcProvider(
     "https://little-frosty-field.xdai.quiknode.pro/25b211172476a7b94f4d1083a740c4c2fc325b0e/",
@@ -16,6 +18,7 @@ const provider = new ethers.providers.JsonRpcProvider(
 function instance(address: string, abi: Array<string | Fragment | JsonFragment>): Contract {
     return new ethers.Contract(address, abi, provider) as Contract
 }
+
 async function healthCheck(): Promise<void> {
     const results = await fetch(`https://metadata.perp.exchange/production.json`)
     const json = await results.json()
@@ -23,13 +26,6 @@ async function healthCheck(): Promise<void> {
 
     const usdc = instance(layer2["externalContracts"]["usdc"], ERC20Artifact.abi) as ERC20
     const arbitrageur = layer2["externalContracts"]["arbitrageur"]
-
-    const ammETH = instance(layer2["contracts"]["ETHUSDC"]["address"], AmmArtifact.abi) as Amm
-    const ammBTC = instance(layer2["contracts"]["BTCUSDC"]["address"], AmmArtifact.abi) as Amm
-    const ammYFI = instance(layer2["contracts"]["YFIUSDC"]["address"], AmmArtifact.abi) as Amm
-    const ammDOT = instance(layer2["contracts"]["DOTUSDC"]["address"], AmmArtifact.abi) as Amm
-    const ammSNX = instance(layer2["contracts"]["SNXUSDC"]["address"], AmmArtifact.abi) as Amm
-    const ammLINK = instance(layer2["contracts"]["LINKUSDC"]["address"], AmmArtifact.abi) as Amm
     const insuranceFund = instance(
         layer2["contracts"]["InsuranceFund"]["address"],
         InsuranceFundArtifact.abi,
@@ -39,94 +35,45 @@ async function healthCheck(): Promise<void> {
         ClearingHouseArtifact.abi,
     ) as ClearingHouse
 
-    console.log("========= address ============")
-    console.log("insuranceFund", insuranceFund.address)
-    console.log("clearingHouse", clearingHouse.address)
+    const insuranceFundBalance = await usdc.functions.balanceOf(insuranceFund.address)
+    console.log("=========")
+    console.log("# InsuranceFund")
+    console.log("## Proxy Address: ", insuranceFund.address)
+    console.log(`## Balance: ${utils.formatUnits(insuranceFundBalance.toString(), USDC_DECIMALS)}`)
 
-    const ethCap = await ammETH.getOpenInterestNotionalCap()
-    const btcCap = await ammBTC.getOpenInterestNotionalCap()
-    const yfiCap = await ammYFI.getOpenInterestNotionalCap()
-    const dotCap = await ammDOT.getOpenInterestNotionalCap()
-    const snxCap = await ammSNX.getOpenInterestNotionalCap()
-    const linkCap = await ammLINK.getOpenInterestNotionalCap()
-    console.log(`ethCap=$${utils.formatEther(ethCap.toString())}`)
-    console.log(`btcCap=$${utils.formatEther(btcCap.toString())}`)
-    console.log(`yfiCap=$${utils.formatEther(yfiCap.toString())}`)
-    console.log(`dotCap=$${utils.formatEther(dotCap.toString())}`)
-    console.log(`snxCap=$${utils.formatEther(snxCap.toString())}`)
-    console.log(`linkCap=$${utils.formatEther(linkCap.toString())}`)
+    const clearingHouseBalance = await usdc.functions.balanceOf(clearingHouse.address)
+    console.log("=========")
+    console.log("# ClearingHouse")
+    console.log("## Proxy Address: ", clearingHouse.address)
+    console.log(`## Balance: ${utils.formatUnits(clearingHouseBalance.toString(), USDC_DECIMALS)}`)
 
-    const ethPersonalCap = await ammETH.getMaxHoldingBaseAsset()
-    const btcPersonalCap = await ammBTC.getMaxHoldingBaseAsset()
-    const yfiPersonalCap = await ammYFI.getMaxHoldingBaseAsset()
-    const dotPersonalCap = await ammDOT.getMaxHoldingBaseAsset()
-    const snxPersonalCap = await ammSNX.getMaxHoldingBaseAsset()
-    console.log(`ethPersonalCap=${utils.formatEther(ethPersonalCap.toString())}`)
-    console.log(`btcPersonalCap=${utils.formatEther(btcPersonalCap.toString())}`)
-    console.log(`yfiPersonalCap=${utils.formatEther(yfiPersonalCap.toString())}`)
-    console.log(`dotPersonalCap=${utils.formatEther(dotPersonalCap.toString())}`)
-    console.log(`snxPersonalCap=${utils.formatEther(snxPersonalCap.toString())}`)
-    console.log(`linkPersonalCap=${utils.formatEther(snxPersonalCap.toString())}`)
+    const arbBalance = await usdc.functions.balanceOf(arbitrageur)
+    console.log("=========")
+    console.log("# Arbitrager")
+    console.log("## EOA Address: ", arbitrageur)
+    console.log(`## Balance: ${utils.formatUnits(arbBalance.toString(), USDC_DECIMALS)}`)
 
-    const ETHOpenInterest = await clearingHouse.openInterestNotionalMap(ammETH.address)
-    console.log("open interest of ETH", utils.formatEther(ETHOpenInterest.toString()))
-    const BTCOpenInterest = await clearingHouse.openInterestNotionalMap(ammBTC.address)
-    console.log("open interest of BTC", utils.formatEther(BTCOpenInterest.toString()))
-    const YFIOpenInterest = await clearingHouse.openInterestNotionalMap(ammYFI.address)
-    console.log("open interest of YFI", utils.formatEther(YFIOpenInterest.toString()))
-    const DOTOpenInterest = await clearingHouse.openInterestNotionalMap(ammDOT.address)
-    console.log("open interest of DOT", utils.formatEther(DOTOpenInterest.toString()))
-    const SNXOpenInterest = await clearingHouse.openInterestNotionalMap(ammSNX.address)
-    console.log("open interest of SNX", utils.formatEther(SNXOpenInterest.toString()))
-    const LINKOpenInterest = await clearingHouse.openInterestNotionalMap(ammLINK.address)
-    console.log("open interest of LINK", utils.formatEther(LINKOpenInterest.toString()))
-
-    const ETHReserves = await ammETH.getReserve()
-    console.log(
-        `ETH Amm quote reserve=${utils.formatEther(
-            ETHReserves[0].toString(),
-        )}, ETH Amm base reserve=${utils.formatEther(ETHReserves[1].toString())}`,
-    )
-    const BTCReserves = await ammBTC.getReserve()
-    console.log(
-        `BTC Amm quote reserve=${utils.formatEther(
-            BTCReserves[0].toString(),
-        )}, BTC Amm base reserve=${utils.formatEther(BTCReserves[1].toString())}`,
-    )
-    const YFIReserves = await ammYFI.getReserve()
-    console.log(
-        `YFI Amm quote reserve=${utils.formatEther(
-            YFIReserves[0].toString(),
-        )}, YFI Amm base reserve=${utils.formatEther(YFIReserves[1].toString())}`,
-    )
-    const DOTReserves = await ammDOT.getReserve()
-    console.log(
-        `DOT Amm quote reserve=${utils.formatEther(
-            DOTReserves[0].toString(),
-        )}, DOT Amm base reserve=${utils.formatEther(DOTReserves[1].toString())}`,
-    )
-    const SNXReserves = await ammSNX.getReserve()
-    console.log(
-        `SNX Amm quote reserve=${utils.formatEther(
-            SNXReserves[0].toString(),
-        )}, SNX Amm base reserve=${utils.formatEther(SNXReserves[1].toString())}`,
-    )
-    const LINKReserve = await ammLINK.getReserve()
-    console.log(
-        `LINK Amm quote reserve=${utils.formatEther(
-            SNXReserves[0].toString(),
-        )}, LINK Amm base reserve=${utils.formatEther(SNXReserves[1].toString())}`,
-    )
-
-    console.log("========= balance ============")
-    const balanceOfInsuranceFund = (await usdc.functions.balanceOf(insuranceFund.address))[0]
-    const balanceOfClearingHouse = (await usdc.functions.balanceOf(clearingHouse.address))[0]
-    const balanceOfArbitrageur = (await usdc.functions.balanceOf(arbitrageur))[0]
-    console.log("balance of insuranceFund", utils.formatUnits(balanceOfInsuranceFund.toString(), USDC_DECIMALS), "usd")
-    console.log("balance of clearingHouse", utils.formatUnits(balanceOfClearingHouse.toString(), USDC_DECIMALS), "usd")
-    console.log("balance of arbitrageur", utils.formatUnits(balanceOfArbitrageur.toString(), USDC_DECIMALS), "usd")
-
-    console.log("========= misc ============")
+    console.log("=========")
+    console.log("# Amm")
+    const ammAddressList = await insuranceFund.getAllAmms()
+    for (const it of ammAddressList) {
+        const amm = instance(it, AmmArtifact.abi) as Amm
+        const priceFeedKey = ethers.utils.parseBytes32String(await amm.priceFeedKey())
+        const openInterestNotionalCap = await amm.getOpenInterestNotionalCap()
+        const openInterestNotional = await clearingHouse.openInterestNotionalMap(it)
+        const maxHoldingBaseAsset = await amm.getMaxHoldingBaseAsset()
+        const reserve = await amm.getReserve()
+        const quoteAssetReserve = reserve[0]
+        const baseAssetReserve = reserve[1]
+        console.log("--------")
+        console.log(`## Market: ${priceFeedKey}`)
+        console.log(`### Proxy Address: ${it}`)
+        console.log(`### OpenInterestNotionalCap: ${utils.formatEther(openInterestNotionalCap.toString())} USDC`)
+        console.log(`### OpenInterestNotional: ${utils.formatEther(openInterestNotional.toString())} USDC`)
+        console.log(`### MaxHoldingBaseAsset: ${utils.formatEther(maxHoldingBaseAsset.toString())} ${priceFeedKey}`)
+        console.log(`### QuoteAssetReserve: ${utils.formatEther(quoteAssetReserve.toString())} USDC`)
+        console.log(`### BaseAssetReserve: ${utils.formatEther(baseAssetReserve.toString())} ${priceFeedKey}USDC`)
+    }
 }
 
 if (require.main === module) {
