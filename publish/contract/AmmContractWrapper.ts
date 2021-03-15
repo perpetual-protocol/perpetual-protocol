1e67ee98c30b65ef3389ed27f0f6a87331e0f7e1
Chun-Wen Wang <wancw@perp.fi>
2021-03-15 13:49:02 +0800

    fix: use byte32 feed key in fetchPrice()

--- publish/contract/AmmContractWrapper.ts
+++ publish/contract/AmmContractWrapper.ts
@@ -7,7 +7,7 @@ import { Amm, IPriceFeed } from "../../types/ethers"
 import { ContractWrapper } from "./ContractWrapper"
 import { AmmDeployArgs } from "./DeployConfig"
 
-export async function fetchPrice(feedAddress: string, feedKey: string): Promise<BigNumber> {
+async function fetchPrice(feedAddress: string, feedKey: string): Promise<BigNumber> {
     const priceContract = (await ethers.getContractAt("IPriceFeed", feedAddress)) as IPriceFeed
     try {
         return priceContract.getPrice(feedKey)
@@ -32,10 +32,10 @@ export class AmmContractWrapper extends ContractWrapper<Amm> {
             spreadRatio,
         } = ammDeployArgs
 
-        const priceInWei = await fetchPrice(priceFeedAddress, priceFeedKey)
+        const priceFeedKeyBytes = ethers.utils.formatBytes32String(priceFeedKey.toString())
+        const priceInWei = await fetchPrice(priceFeedAddress, priceFeedKeyBytes)
         const updatedQuoteAssetReserve = baseAssetReserve.mul(priceInWei).div(BigNumber.from(10).pow(18))
 
-        const priceFeedKeyBytes = ethers.utils.formatBytes32String(priceFeedKey.toString())
         const args = [
             updatedQuoteAssetReserve.toString(),
             baseAssetReserve.toString(),
