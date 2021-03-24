5180aaaa64c6e811d80c401affd62f45d17cb516
shao <wraecca@gmail.com>
2021-03-24 13:16:18 +0800

    update staking deploy script

--- publish/migrations/0020-layer1-deploy-PerpRewardVesting.ts
+++ publish/migrations/0020-layer1-deploy-PerpRewardVesting.ts
@@ -2,22 +2,23 @@
 
 import { BigNumber } from "ethers"
 import { PerpRewardVesting } from "../../types/ethers"
-import { ContractFullyQualifiedName } from "../ContractName"
+import { ContractFullyQualifiedName, ContractInstanceName } from "../ContractName"
 import { MigrationContext, MigrationDefinition } from "../Migration"
 
 const migration: MigrationDefinition = {
     getTasks: (context: MigrationContext) => [
         async (): Promise<void> => {
-            console.log("deploy PerpRewardVesting with 0 vesting...")
-            await context.factory
-                .create<PerpRewardVesting>(ContractFullyQualifiedName.PerpRewardVesting)
-                .deployUpgradableContract(context.externalContract.perp!, 0)
-        },
-        async (): Promise<void> => {
-            console.log("deploy PerpRewardVesting with 5 minutes vesting...")
-            await context.factory
-                .create<PerpRewardVesting>(ContractFullyQualifiedName.PerpRewardVesting)
-                .deployUpgradableContract(context.externalContract.perp!, BigNumber.from(60 * 5))
+            if (context.stage === "production") {
+                console.log("deploy PerpRewardVesting with 182 days (half year) vesting...")
+                await context.factory
+                    .create<PerpRewardVesting>(ContractFullyQualifiedName.PerpRewardVesting, ContractInstanceName.PerpStakingReward182DaysVesting)
+                    .deployUpgradableContract(context.externalContract.perp!, BigNumber.from(60 * 60 * 24 * 182))
+            } else {
+                console.log("deploy PerpRewardVesting with 5 mins vesting...")
+                await context.factory
+                    .create<PerpRewardVesting>(ContractFullyQualifiedName.PerpRewardVesting, ContractInstanceName.PerpStakingReward5MinutesVesting)
+                    .deployUpgradableContract(context.externalContract.perp!, BigNumber.from(60 * 5))
+            }
         },
     ],
 }
